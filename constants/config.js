8a0c9f98821821802e795daa7339892e4d2e71c7
Ravi Raj <91177341+Ravi192003Raj@users.noreply.github.com>
2024-07-28 14:05:38 +0530

    Update config.js

--- constants/config.js
+++ constants/config.js
@@ -1,10 +1,5 @@
 const corsOptions = {
-  origin: [
-    "http://localhost:5173",
-    "http://localhost:4173",
-    "https://snappychatapp-73l823nyd-ravi-rajs-projects-f79ece4d.vercel.app",
-    process.env.CLIENT_URL,
-  ],
+  origin: process.env.CLIENT_URL,
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
 };
